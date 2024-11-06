fc1091572404b7c77d9cd1703bac97a6b7728412
Pushkar Raj <px86@protonmail.com>
2024-11-06 18:38:55 +0530

    Gracefully shutdown MqttAsyncClient

--- devices/thermostat/src/main/java/io/github/px86/iothomecontrol/devices/thermostat/MqttClientConfig.java
+++ devices/thermostat/src/main/java/io/github/px86/iothomecontrol/devices/thermostat/MqttClientConfig.java
@@ -4,6 +4,8 @@ import org.eclipse.paho.mqttv5.client.IMqttToken;
 import org.eclipse.paho.mqttv5.client.MqttAsyncClient;
 import org.eclipse.paho.mqttv5.client.MqttConnectionOptions;
 import org.eclipse.paho.mqttv5.common.MqttException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +18,8 @@ public class MqttClientConfig {
 
   @Value("${mqtt.client.id}")
   private String clientId;
+
+  private static Logger log = LoggerFactory.getLogger(MqttClientConfig.class);
 
   @Bean
   public MqttConnectionOptions mqttConnectionOptions() {
@@ -33,6 +37,28 @@ public class MqttClientConfig {
     MqttAsyncClient client = new MqttAsyncClient(brokerUrl, clientId);
     IMqttToken token = client.connect(mqttConnectionOptions());
     token.waitForCompletion();
+
+    addShutdownHook(client);
+
     return client;
+  }
+
+  private void addShutdownHook(final MqttAsyncClient client) {
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  if (client.isConnected()) {
+                    try {
+                      log.info("disconnecting the client...");
+                      client.disconnect().waitForCompletion();
+                      log.info("client disconnected.");
+                    } catch (MqttException exception) {
+                      log.error(
+                          "exception occured while trying to disconnect the client: {}",
+                          exception.getMessage());
+                    }
+                  }
+                }));
   }
 }
