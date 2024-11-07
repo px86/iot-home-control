46d33e8f8b38422eab6dab23644d77f62c4c2598
Pushkar Raj <px86@protonmail.com>
2024-11-07 10:42:02 +0530

    Add project for device-registry

---
+++ device-registry/src/main/java/io/github/px86/iothomecontrol/deviceregistry/IotHomeControlDeviceRegistryApplication.java
@@ -0,0 +1,13 @@
+package io.github.px86.iothomecontrol.deviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotHomeControlDeviceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotHomeControlDeviceRegistryApplication.class, args);
+	}
+
+}
---
+++ device-registry/src/test/java/io/github/px86/iothomecontrol/deviceregistry/IotHomeControlDeviceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.px86.iothomecontrol.deviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotHomeControlDeviceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
